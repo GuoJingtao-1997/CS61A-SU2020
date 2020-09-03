20a4bae03639fefb6700bcdec0e2387030b20e04
GuoJingtao-1997 <guojingtao1997@163.com>
2020-09-03 17:28:54 +0800

    update linked-list

--- disc/linked-list.py
+++ disc/linked-list.py
@@ -20,3 +20,64 @@ def rest(s):
     assert is_link(s), "rest only applies to linked lists."
     assert s != empty, "empty linked list has no rest."
     return s[1]
+
+def len_link(s):
+    """Return the length of linked list s."""
+    length = 0
+    while s != empty:
+        s, length = rest(s), length + 1
+    return length
+
+def len_link_recursive(s):
+    """use recursion to return the length of linked list s."""
+    if s == empty:
+        return 0
+    return 1 + len_link_recursive(rest(s))
+
+def getitem_link(s, i):
+    """Return the element at index i of linked list s."""
+    while i > 0:
+        s, i = rest(s), i - 1
+    return first(s)
+
+def getitem_link_recursive(s, i):
+    """use recursion to return the element at index i of linked list s."""
+    if i == 0:
+        return first(s)
+    return getitem_link_recursive(rest(s), i - 1)
+
+def extend_link(s, t):
+    """Return a list with the elements of s followed by those of t."""
+    assert is_link(s) and is_link(t)
+    if s == empty:
+        return t
+    else:
+        return link(first(s), extend_link(rest(s), t))
+
+def apply_to_all_link(f, s):
+    """Apply f to each element of s."""
+    assert is_link(s)
+    if s == empty:
+        return s
+    else:
+        return link(f(first(s)), apply_to_all_link(f, rest(s)))
+
+def keep_if_link(f, s):
+    """Return a list with elements of s for which f(e) is true."""
+    assert is_link(s)
+    if s == empty:
+        return s
+    else:
+        kept = keep_if_link(f, rest(s))
+        if f(first(s)):
+            return link(first(s), kept)
+        else:
+            return kept
+
+def join_link(s, separator):
+    """Return a string of all elements in s separated by separator."""
+    if s == empty:
+        return ""
+    elif rest(s) == empty:
+        return str(first(s))
+    return str(first(s)) + separator + join_link(rest(s), separator)
