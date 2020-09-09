cfefa85ecacda392da7887018905eed5eb827ce0
GuoJingtao-1997 <guojingtao1997@163.com>
2020-09-09 18:08:43 +0800

    finish lab08

---
+++ lab/lab08/lab08.py
@@ -0,0 +1,42 @@
+def make_generators_generator(g):
+    """Generates all the "sub"-generators of the generator returned by
+    the generator function g.
+
+    >>> def every_m_ints_to(n, m):
+    ...     i = 0
+    ...     while (i <= n):
+    ...         yield i
+    ...         i += m
+    ...
+    >>> def every_3_ints_to_10():
+    ...     for item in every_m_ints_to(10, 3):
+    ...         yield item
+    ...
+    >>> for gen in make_generators_generator(every_3_ints_to_10):
+    ...     print("Next Generator:")
+    ...     for item in gen:
+    ...         print(item)
+    ...
+    Next Generator:
+    0
+    Next Generator:
+    0
+    3
+    Next Generator:
+    0
+    3
+    6
+    Next Generator:
+    0
+    3
+    6
+    9
+    """
+    def gen_helper(num_next):
+        gen = g()
+        for _ in range(num_next):
+            yield next(gen)
+    i, gen_lst = 1, list(g())
+    for _ in gen_lst:
+        yield gen_helper(i)
+        i += 1
